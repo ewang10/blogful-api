18f3a6e7d945d4528b1ce9617a36cda4c29716e1
Eric Wang <ericwang27759@gmail.com>
2019-11-08 22:24:18 -0500

    Testing GET /articles step 5 problem

--- src/app.js
+++ src/app.js
@@ -4,6 +4,8 @@ const morgan = require('morgan');
 const cors = require('cors');
 const helmet = require('helmet');
 const {NODE_ENV} = require('./config');
+const ArticleService = require('./article-service');
+
 const app = express();
 
 const morganOption = (NODE_ENV === 'production')
@@ -13,6 +15,21 @@ const morganOption = (NODE_ENV === 'production')
 app.use(morgan(morganOption));
 app.use(helmet());
 app.use(cors());
+
+app.get('/articles', (req, res, next) => {
+    const knexInstance = req.app.get('db');
+    ArticleService.getAllArticles(knexInstance)
+        .then(articles => {
+            res.json(articles.map(article => ({
+                id: article.id,
+                title: article.title,
+                style: article.style,
+                content: article.content,
+                date_published: new Date(article.date_published)
+            })))
+        })
+        .catch(next);
+});
 
 app.get('/', (req, res) => {
     res.send('Hello, world!');
